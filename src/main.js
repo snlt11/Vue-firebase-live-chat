6dc9a42191cda795e941e6e7c279e41bef0e7419
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2024-04-11 20:46:39 +0630

    fix: guard&route

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/global.css";
+import { auth } from "@/firebase/config";
 
-const app = createApp(App);
+let app;
 
-app.use(router);
-
-app.mount("#app");
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App).use(router).mount("#app");
+  }
+});
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,9 +5,22 @@
 </template>
 
 <script>
+import { watch } from "vue";
 import Navbar from "../components/Navbar.vue";
+import getUser from "@/composable/getUser";
+import { useRouter } from "vue-router";
 export default {
   components: { Navbar },
+  setup() {
+    let { user } = getUser();
+    let router = useRouter();
+
+    watch(user, () => {
+      if (!user.value) {
+        router.push("/");
+      }
+    });
+  },
 };
 </script>
 
